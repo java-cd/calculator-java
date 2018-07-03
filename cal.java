5dd9b58aad61f2564362284c390aacd9f178f5db
java-cd <40804109+java-cd@users.noreply.github.com>
2018-07-03 13:42:56 -0400

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,10 @@
+class cal { 
+public static void main( String[] args) {
+if (args[0].equals("addition")) { 
+double num1 = Integer.parseInt(args[1]);
+double num2 = Integer.parseInt(args[2]);
+double sum = num1 + num2;
+System.out.println(sum);
+}
+}
+}
